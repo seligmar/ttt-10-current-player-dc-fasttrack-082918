f573872afb7a3dfb188ed8c25f7a49d40c69806f
Mary Selig <seligmar@gmail.com>
2018-09-30 19:15:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
 def badge_maker(name)
-  puts "Hello, my name is #{batch_badge_creator}."
+  puts "Hello, my name is #{name}."
+  while name = 
 end 
 
 batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
