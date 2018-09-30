71368a95f0139a938ac88884f87c8294494eee91
Mary Selig <seligmar@gmail.com>
2018-09-30 19:23:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name[""]}."
+  puts "Hello, my name is #{name}."
 end 
 
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
